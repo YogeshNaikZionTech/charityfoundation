bf569ebcab9a4045381d3664e839619f4c9cd75f
vJay ReddY <vJay ReddY>
2016-09-20 10:32:57 -0700

    Fixed footer issue and updated contactus page with google maps

--- resources/views/contactus/create.blade.php
+++ resources/views/contactus/create.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', '|Contact Us')
 @section('stylesheets')
+	<link href="{{URL::asset('/css/contactus.css')}}" rel="stylesheet" type="text/css"/>
 
 @endsection
 @section('content')
@@ -11,7 +12,7 @@
                 <strong>Thank you {{$contactuser->name}} {{\Session::get('contact_Success')}}</strong>
             </div>
         @endif
-        <div class="fa-header">
+        <div>
             <h2>Contact</h2>
             <hr class="container col-lg-11">
         </div>
@@ -67,29 +68,35 @@
                 If you are interested in volunteering for an event, share your availability so we will get in touch with you. </p>
         </div>
         <div class="container " >
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-11">
-                <form class="form-horizontal" name="contact" action="{{url('contact')}}" method="post">
-                    <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12" style="margin-right:3px;">
-                        <input  name="name" required type="text" class="form-control col-lg-6 col-md-6 col-sm-6 col-xs-10" placeholder="Name *" />
-                    </div>
-
-                    <!--</form>-->
-                    <!--<form class="form-horizontal">-->
-                    <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <input  name="email" required type="email" class="form-control col-lg-6 col-md-6 col-sm-6 col-xs-10" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" placeholder="Email *" />
-                    </div>
-
+	<div class="container form-data " >
+        <form class="form-horizontal col-lg-12 col-md-12 col-sm-12 col-xs-11" name="contact" action="{{url('contact')}}" method="post" >
+            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-10 contact">
+            <div class="form-group col-lg-10 col-md-10 col-sm-10 col-xs-11 pull-right " >
+                <input  name="name" required type="text" class="form-control col-lg-6 col-md-6 col-sm-6 col-xs-10" placeholder="Name *" />
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-11">
 
-                    <div class="form-group col-lg-10 col-md-10 col-sm-12 col-xs-12">
-                    <textarea  name="message"  class="form-control col-lg-10 col-md-10 col-sm-10 col-xs-10" placeholder="Suggest a new project or event or share your availability timing for volunteer work" rows="9">
+            <div class="form-group col-lg-10 col-md-10 col-sm-10 col-xs-11  pull-right">
+                <input  name="email" required type="email" class="form-control col-lg-6 col-md-6 col-sm-6 col-xs-10" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" placeholder="Email *" />
+            </div>
+                <div class="form-group col-lg-10 col-md-10 col-sm-10 col-xs-11 pull-right" >
+                    <textarea  name="message"  class="form-control col-lg-10 col-md-10 col-sm-10 col-xs-10" placeholder="We Need your Suggestions" rows="9">
                     </textarea>
-                    </div>
-                <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <input type="submit" class="btn btn-success" value="Submit">
-                </form>
+                </div>
             </div>
+            <div class="container maps col-lg-7 col-md-7 col-sm-7 col-xs-10">
+                   <iframe class="  col-lg-11 col-md-11 col-sm-11 col-xs-10"   src="http://www.maps.ie/create-google-map/map.php?width=100%&amp;height=400&amp;hl=en&amp;coord=37.3876633,-121.93013960000002&amp;q=2665%20N%20First%20street+(Charity%20Foundation)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                       <a href="http://www.mapsdirections.info/nl/maak-een-google-map/">Maak een Google Map</a>
+                       <a href="http://www.mapsdirections.info/nl/">Bereken route</a></iframe>
+               </div><br />
+
+        </form>
+
+    	</div>
+	 <div class="submit-form col-lg-6 col-xs-10 col-sm-6 col-md-6">
+		<input type="hidden" name="_token" value="{{csrf_token()}}">
+               
+    	<button class="btn btn-success btn-lg col-lg-offset-6 col-sm-offset-6 col-xs-offset-4 col-md-offset-6 " type="submit">Submit</button>
+    	</div>
         </div>
 
     </div>
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,11 +1,54 @@
 
-    <footer>
-
+ <footer>
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3 col-lg-3 col-xs-10 col-sm-3">
                 <span class="copyright">Copyright &copy; Charity Foundation 2016</span>
+                    <ul class="list-inline quicklinks">
+                        <li><a href="#">Privacy Policy</a>
+                        </li>
+                        <li><a href="#">Terms of Use</a>
+                        </li>
+                    </ul>
+                </div>
+
+            <div class="col-md-2 col-lg-2 col-xs-10 col-sm-3">
+                <label class="footer-label">Projects</label>
+                <ol>
+                    <li>
+                        <a href="#">Project 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Event 1</a>
+                    </li>
+                </ol>
             </div>
-            <div class="col-md-4">
+             <div class="col-md-2 col-lg-2 col-xs-10 col-sm-3">
+                <label class="footer-label">Register</label>
+                <ol>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                    <li>
+                        <a href="#">Sign Up</a>
+                    </li>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact US</a>
+                    </li>
+                </ol>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xs-10 col-sm-3">
+                <label class="footer-label">Address</label>
+                <p>
+                    Charity Foundation<br/>
+                    2665 N Frist St,<br/>
+                    San Jose, CA, 95134
+                </p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xs-10 col-sm-3">
                 <ul class="list-inline social-buttons">
                     <li><a href="#"><i class="fa fa-twitter"></i></a>
                     </li>
@@ -15,14 +58,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-md-4">
-                <ul class="list-inline quicklinks">
-                    <li><a href="#">Privacy Policy</a>
-                    </li>
-                    <li><a href="#">Terms of Use</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-</footer>
 
+        </div>
+    </div>
+</footer>
