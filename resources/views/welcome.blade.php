39677a5578bedf454fb61697fb126c8bf3952704
kimsuka <kimsukar@ziontech.com>
2016-09-23 03:27:52 -0700

    total no of donations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,13 @@ $(document).ready(function(){
 });
     </script>
     <div id="content">
-    <div class="bs-example">
+
+    <div class="container" id="quotes">
+
+        <blockquote><h2>To ease another’s heartache is to forget one’s own.<div id="author">-Abraham Lincoln</div></h2></blockquote>
+    </div>
+
+<!--     <div class="bs-example">
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
        
         <ol class="carousel-indicators">
@@ -46,14 +52,14 @@ $(document).ready(function(){
             <span class="glyphicon glyphicon-chevron-right"></span>
         </a>
     </div>
-</div>
-
+</div> -->
+<hr style="border-top: 1px solid black; margin-top: 75px; width: 100%">
         <div class="container div2">
             <div class="row">
-                <div class="col-md-12">
-                    <div class="row">
-                        @foreach($projects_c as $cproject)
-                            <div class="col-md-3">
+                <div class="col-md-9">
+                    
+<!--                         @foreach($projects_c->slice(0,3) as $cproject)
+                            <div class="col-md-4">
                                 <div class="panel panel-primary">
                                     <div class="panel-heading">{{$cproject->project_Status}}</div>
                                     <div class="panel-body"><img src="images/{{$cproject->project_Image}}" class="img-responsive" style="width:100%" alt="Image" height="50px"></div>
@@ -61,12 +67,12 @@ $(document).ready(function(){
        <div class="panel-footer"><button class="btn btn-success btn-md"  title="Current Event title" data-toggle="popover" data-placement="right" data-content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy ">See more </button></div>                                
                                 </div>
                             </div>
-                        @endforeach
-                    </div>
+                        @endforeach -->
+                    
 
 
-                        @foreach($projects_f as $fproject)
-                            <div class="col-md-3">
+                        @foreach($projects_f->slice(0,3) as $fproject)
+                            <div class="col-md-4">
                                 <div class="panel panel-primary">
                                     <div class="panel-heading">{{$fproject->project_Status}}</div>
                                     <div class="panel-body"><img src="images/{{$fproject->project_Image}}" class="img-responsive" style="width:100%" alt="Image" height="50px"></div>
@@ -74,11 +80,11 @@ $(document).ready(function(){
                                 </div>
                             </div>
                         @endforeach
-                    </div>
+                    
 
 
-                                @foreach($projects_p as $pproject)
-                                    <div class="col-md-3">
+                                @foreach($projects_p->slice(0,3) as $pproject)
+                                    <div class="col-md-4">
                                         <div class="panel panel-primary">
                                             <div class="panel-heading">{{$pproject->project_Status}}</div>
                                             <div class="panel-body"><img src="images/{{$pproject->project_Image}}" class="img-responsive" style="width:100%" alt="Image" height="50px"></div>
@@ -87,7 +93,35 @@ $(document).ready(function(){
                                         </div>
                                     </div>
                                 @endforeach
+
                             </div>
+
+
+
+                                <div class="col-md-3 donors btn btn-success">Total Number of Donations<hr style="margin:0px"> 
+                                <div style="padding-bottom:5px"><span style="font-size:40px; padding-bottom:5px"> 123,456,789</span></div>   
+                                  </div>
+                                <div class="col-md-3 suggest"><h2>Suggestions</h2><hr style="margin:0px"> 
+                                <textarea style="border-radius:4px;color:black"rows="8" cols="40" placeholder="We would like to hear your ideas"></textarea> 
+                                <button class="btn btn-success" type="submit" id="suggest">Submit</button>  
+                                  </div>
+                                </div>
+                                </div>
+
+
+
+
+  
+                                <!-- <div class="row" style="border: #ddd 1px solid;">
+
+                                    <div class="col-md-6 suggest">  
+                                    <textarea rows="10" cols="100">  </textarea>
+                                    
+                                    </div>
+<div class="col-md-3 col-md-offset-1 donor">Total Number of Donations<hr style="margin:0px"> 
+                                <div style="padding-bottom:5px"><span style="font-size:40px; padding-bottom:5px"> 123,456,789</span></div>   
+                                  </div>
+                                </div>  -->
                 </div>
             </div>
     </div>
