0942fa7571c45ec8702bd8cbb03fee92400f687b
sravanthirk <sravanthik@ziontech.com>
2016-09-23 15:03:21 -0700

    add donate route

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -30,7 +30,7 @@
                    --><label>Project Description</label>
                     <textarea placeholder="Description of the project" class="form-control"></textarea>
                   <label>Upload an image</label>
-                    <input type="file" name="pic" accept="image/*">
+                    <input type="file" Name="pic" accept="image/*">
                 </div>
                 <div class="modal-footer">
                   <input type="submit" class="btn btn-success" data-dismiss="modal">
@@ -57,11 +57,11 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/upcoming.jpg')}}" alt="event1">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName1"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-default" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Donate</a>
+                                            <a href="{{url('/donates/create')}}" class="btn btn-primary pull-right donate" id="Project 1" role="button">Donate</a>
                                         </p>
                                     </div>
                                 </div>
@@ -70,11 +70,11 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/upcoming.jpg')}}" alt="event2">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName2"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-default" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Donate</a>
+                                            <a href="{{url('/donates/create')}}" class="btn btn-primary pull-right donate" id="Project 2" role="button">Donate</a>
                                         </p>
                                     </div>
                                 </div>
@@ -83,11 +83,11 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/upcoming.jpg')}}" alt="event3">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName3"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-default" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Donate</a>
+                                            <a href="{{url('/donates/create')}}" class="btn btn-primary pull-right donate" id="Project 3" role="button">Donate</a>
                                         </p>
                                     </div>
                                 </div>
@@ -96,16 +96,16 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/upcoming.jpg')}}" alt="event3">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName4"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-default" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Donate</a>
+                                            <a href="{{url('/donates/create')}}" class="btn btn-primary pull-right donate" id="Project 4" role="button">Donate</a>
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="projectDesc" id="desc">
+                          <!--   <div class="projectDesc" id="desc">
                                 <h2>Project Name</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -115,7 +115,7 @@
                                     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                                 <h3>Venue</h3>
                                 <h3>Date & Time</h3>
-                            </div>
+                            </div> -->
                             <!--  -->
                         </div>
 
@@ -124,11 +124,11 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/event.jpg')}}" alt="event1">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName1"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-primary" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Read More</a>
+                                            <a href="#desc" class="btn btn-primary pull-right" role="button" id="read1">Read More</a>
                                         </p>
                                     </div>
                                 </div>
@@ -137,11 +137,11 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/event.jpg')}}" alt="event2">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName2"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-primary" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Read More</a>
+                                            <a href="#desc" class="btn btn-primary pull-right" role="button" id="read2">Read More</a>
                                         </p>
                                     </div>
                                 </div>
@@ -150,11 +150,11 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/event.jpg')}}" alt="event3">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName3"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-primary" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Read More</a>
+                                            <a href="#desc" class="btn btn-primary pull-right" role="button" id="read3">Read More</a>
                                         </p>
                                     </div>
                                 </div>
@@ -163,16 +163,16 @@
                                 <div class="thumbnail">
                                     <img src="{{URL::asset('/images/event.jpg')}}" alt="event3">
                                     <div class="caption">
-                                        <a href="#desc" class="eNAme"><h3>Project Name</h3></a>
+                                        <a href="#desc" class="eName4"><h3>Project Name</h3></a>
                                         <p>Short summary of project description.</p>
                                         <p>
                                             <a href="#" class="btn btn-primary" role="button" style="visibility:hidden"></a>
-                                            <a href="#" class="btn btn-primary pull-right" role="button">Read More</a>
+                                            <a href="#desc" class="btn btn-primary pull-right" role="button" id="read4">Read More</a>
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="projectDesc" id="desc">
+                            <!-- <div class="projectDesc" id="desc">
                                 <h2>Project Name</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -182,8 +182,50 @@
                                     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                                 <h3>Venue</h3>
                                 <h3> Date & Time</h3>
-                            </div>
+                            </div> -->
                         </div>
+ <div class="eventDesc1" id="desc">
+                    <h2>Project Name 1</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
+               </div>
+                <div class="eventDesc2" id="desc">
+                    <h2>Project Name 2</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    
+               </div> 
+               <div class="eventDesc3" id="desc">
+                    <h2>Project Name 3</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                   
+               </div> 
+               <div class="eventDesc4" id="desc">
+                    <h2>Project Name 4</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                   
+               </div>
+
+
                     </div>
              </div>
        </div>
@@ -202,11 +244,97 @@
 $('.nav li:first').removeClass('active');
 $('.nav li:nth-child(3)').addClass('active');
 
-                    $(".projectDesc").hide();
-                    $(".eNAme").click(function(){
-                        $(".projectDesc").toggle();
-                    });
+$('.donate').on('click', function () {
+    sessionStorage.removeItem('event');
+    var projectValue = $(this).attr('id');
+    sessionStorage.setItem('project', projectValue);
+});
+ $(".eventDesc1").hide();
+        $(".eventDesc2").hide();
+        $(".eventDesc3").hide();
+        $(".eventDesc4").hide();
+
+
+        $(".eName1").click(function(){
+           $(".eventDesc2").hide();
+        $(".eventDesc3").hide();
+        $(".eventDesc4").hide();
+
+            if($(".eventDesc1").attr('style') == "display: none;"){
+                  $(".eventDesc1").show();
+            }
+            else
+                $(".eventDesc1").hide();
+                     });
+
+
+        $(".eName2").click(function(){
+        $(".eventDesc1").hide();
+        $(".eventDesc3").hide();
+        $(".eventDesc4").hide();
+
+
+            if($(".eventDesc2").attr('style') == "display: none;"){
+                  $(".eventDesc2").show();
+            }
+            else
+                $(".eventDesc2").hide();
+                     });
+
+
+        $(".eName3").click(function(){
+
+        $(".eventDesc1").hide();
+        $(".eventDesc2").hide();
+        $(".eventDesc4").hide();
+
+            if($(".eventDesc3").attr('style') == "display: none;"){
+                  $(".eventDesc3").show();
+            }
+            else
+                $(".eventDesc3").hide();
+                     });
+
+  $(".eName4").click(function(){
+
+        $(".eventDesc1").hide();
+        $(".eventDesc2").hide();
+        $(".eventDesc3").hide();
+
+            if($(".eventDesc4").attr('style') == "display: none;"){
+                  $(".eventDesc4").show();
+            }
+            else
+                $(".eventDesc4").hide();
+                     });
+
+$('#read1').click(function(){
+    $(".eventDesc2").hide();
+        $(".eventDesc3").hide();
+                $(".eventDesc4").hide();
+
+  $(".eventDesc1").show();
+});
+$('#read2').click(function(){
+    $(".eventDesc1").hide();
+        $(".eventDesc3").hide();
+                $(".eventDesc4").hide();
+
+  $(".eventDesc2").show();
+});
+$('#read3').click(function(){
+  $(".eventDesc1").hide();
+        $(".eventDesc2").hide();
+                $(".eventDesc4").hide();
+
+  $(".eventDesc3").show();
+});
+$('#read4').click(function(){
+  $(".eventDesc1").hide();
+        $(".eventDesc2").hide();
+                $(".eventDesc3").hide();
+  $(".eventDesc4").show();
+});
                 });
             </script>
     @endsection
-
