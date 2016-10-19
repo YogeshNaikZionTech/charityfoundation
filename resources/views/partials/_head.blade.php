31d8febc019ee3d6d16fd4f64b33fd71659b64f2
preethi0309 <preethig@ziontech.com>
2016-10-18 22:03:57 -0700

    animation speed , added a favicon , change the title to AA Foundation for every page , text alignment in carousel

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,9 +1,10 @@
 
-    <title>Charity @yield('title')</title>
+    <title>AA Foundation @yield('title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet"  type="text/css" href="{{ URL::asset('css/footer.css')}}" >
     <link rel="stylesheet"  type="text/css" href="{{ URL::asset('css/nav.css')}}" >
+    <link rel='shortcut icon' href="{{URL::asset('/images/AAfoundation.png')}}" type='image/x-icon'/>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,66 +20,53 @@
             </ol>
             <div class="carousel-inner" role="listbox">
                 <div class="item active ">
-                    <img class="first-slide" src="{{URL::asset('/images/education3.jpg')}}" alt="First slide">
+                    <img class="first-slide img-responsive" src="{{URL::asset('/images/education3.jpg')}}" alt="First slide">
                     <div class="container">
                         <div class="carousel-caption first" style="position: absolute">
                             <h1>CAUSE1: Education benefit society</h1>
                             <p>"Well-educated citizens are better-equipped for significant economic production. "</p>
-                            <a class="btn btn-lg" href="#" role="button">Read More</a>
+                            <a class="btn btn-lg" href="{{url('projects')}}" role="button">Read More</a>
 
                         </div>
                         <div class="donate_carousel col-md-4  col-lg-4 col-sm-4 col-xs-10 pull-right" >
                             {{--<h2>Urgent Cause</h2>--}}
-                            <div class="donors1 col-md-10 col-lg-10 col-xs-10 col-sm-10" style="padding-bottom: 30px;">
-                                <h2>Total Number of Donors</h2>
+                            <div class="donors1 col-md-10 col-lg-10 col-xs-10 col-sm-10" >
+                                <p>Total Number of Donors</p>
                                 <span class="count">123</span>
 
                             </div>
-                            <button class="btn btn-lg"  style="border: 2px white solid;">DONATE</button>
-                            {{--<div class="circle" id="circles-309">--}}
-                                {{--<div class="circles-wrp" style="position: relative; display: inline-block;">--}}
-                                    {{--<svg xmlns="http://www.w3.org/2000/svg" width="166" height="166">--}}
-                                        {{--<path fill="transparent" stroke="#FFF" stroke-width="10" d="M 82.98411349011178 5.000001617828204 A 78 78 0 1 1 82.89165956483042 5.000075241381751 Z" class="circles-maxValueStroke"></path>--}}
-                                        {{--<path fill="transparent" stroke="#f8b864" stroke-width="10" d="M 82.98411349011178 5.000001617828204 A 78 78 0 0 1 87.81704624539476 5.148885265079684 " class="circles-valueStroke"></path></svg>--}}
-                                    {{--<div class="circles-text" style="position: absolute; top: 0px; left: 0px; text-align: center; width: 100%; font-size: 58.1px; height: 166px; line-height: 166px;">--}}
-                                        {{--<div class="text-inner">0<span class="small">%</span></div>--}}
-                                        {{--<h4>Raised   |    Goal</h4>--}}
-                                        {{--<button class="btn btn-md">DONATE</button>--}}
-                                    {{--</div>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-
+                            <a href="{{url('/select')}}" class="btn btn-lg">DONATE</a>
 
                         </div>
 
                     </div>
                 </div>
                 <div class="item">
-                    <img class="second-slide" src="{{URL::asset('/images/slider6.jpg')}}" alt="Second slide">
+                    <img class="second-slide img-responsive" src="{{URL::asset('/images/slider6.jpg')}}" alt="Second slide">
                     <div class="container">
-                        <div class="carousel-caption second" >
+                        <div class="carousel-caption second col-md-4  col-lg-4 col-sm-4 col-xs-10" >
                             <h1 >CAUSE2:Solar panels to Schools</h1>
                             <p>We make a LIVING by what we GET but we make a LIFE by what we GIVE</p>
-                            <a class="btn btn-lg" href="#" role="button">Read More</a>
+                            <a class="btn btn-lg" href="{{url('projects')}}" role="button">Read More</a>
                         </div>
                         <div class="donate_carousel col-md-4  col-lg-4 col-sm-4 col-xs-10 pull-right" >
                             {{--<h2>Urgent Cause</h2>--}}
-                            <div class="donors1 col-md-10 col-lg-10 col-xs-10 col-sm-10" style="padding-bottom: 30px;">
-                                <h2>Founder can take care of all the administrative cost</h2>
+                            <div class="donors1 col-md-10 col-lg-10 col-xs-10 col-sm-10">
+                                <p>Founder take care of all the administrative cost</p>
 
                             </div>
-                            <button class="btn btn-lg">DONATE</button>
+                            <a href="{{url('/select')}}" class="btn btn-lg">DONATE</a>
                         </div>
 
                     </div>
                 </div>
                 <div class="item">
-                    <img class="third-slide" src="{{URL::asset('/images/education2.jpg')}}" alt="Third slide">
+                    <img class="third-slide img-responsive" src="{{URL::asset('/images/education2.jpg')}}" alt="Third slide">
                     <div class="container">
                         <div class="carousel-caption third">
                             <h1>CAUSE3:Digitize Classrooms</h1>
                             <p>We rise by "LIFTING OTHERS"</p>
-                            <a class="btn btn-lg" href="#" role="button">Read More</a>
+                            <a class="btn btn-lg" href="{{url('projects')}}" role="button">Read More</a>
                         </div>
                     </div>
                 </div>
@@ -108,7 +95,7 @@
             <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4 pull-left">
                 <h2>Video title</h2>
                 <p>Some description  about video ,Main theme of the video</p>
-
+                <a href="{{url('/education')}}" class="btn btn-lg">See more</a>
             </div>
 
         </div>
@@ -126,13 +113,11 @@
                                 <div class="head">{{$cproject->project_Status}}</div>
                                 <div class="project_image image">
                                     <img src="images/{{$cproject->project_Image}}" class="img-responsive" style="width:100%" alt="Image" height="50px"></div>
-
-                                <div>
-                                </div>
                                     {{--<div><h3>Project Name</h3></div>--}}
                                     <div><h3>{{$cproject->project_Title}}</h3></div>
-                                <div><p>A community of lifelong learners, and champions of our own success.</p></div>
-                                    <button class="btn btn-lg"  title={{$cproject->project_Title}} data-content={{$cproject->project_Description}}>See more </button></div>
+                                    <div><p>A community of lifelong learners, and champions of our own success.</p></div>
+                                    <div><a href="{{url('projects')}}" class="btn btn-lg"  title={{$cproject->project_Title}} data-content={{$cproject->project_Description}}>See more </a></div>
+                            </div>
                         </div>
                     @endforeach
                 </div>
@@ -153,7 +138,7 @@
                                 <div><h3>Event Name</h3></div>
                                 <div><p>A community of lifelong learners, and champions of our own success.</p></div>
 
-                                <button class="btn btn-lg">See more </button></div>
+                                <a href="{{url('events')}}" class="btn btn-lg">See more </a></div>
                         </div>
                     </div>
                     <div class="col-md-4 col-lg-4 col-sm-4 col-xs-12">
@@ -166,7 +151,7 @@
                                 <div><h3>Event Name</h3></div>
                                 <div><p>A community of lifelong learners, and champions of our own success.</p></div>
 
-                                <button class="btn btn-lg">See more </button></div>
+                                <a href="{{url('events')}}" class="btn btn-lg">See more </a></div>
                         </div>
                     </div>
                     <div class="col-md-4 col-lg-4 col-sm-4 col-xs-12">
@@ -177,7 +162,7 @@
                             <div>
                                 <div><h3>Event Name</h3></div>
                                 <div><p>A community of lifelong learners, and champions of our own success.</p></div>
-                                <button class="btn btn-lg">See more </button></div>
+                                <a href="{{url('events')}}" class="btn btn-lg">See more </a></div>
                         </div>
                     </div>
 
@@ -212,20 +197,23 @@
 @endsection
 @section('scripts')
      <script>
+         $(document).ready(function(){
+             $(this).scrollTop(0);
+         });
         $(document).ready(function(){
-            var scroll_start = 5;
+            var scroll_start = 1;
             var startChange = $('.nav1');
             var offset = startChange.offset();
             $(document).scroll(function() {
-                scroll_start = $(this).scrollTop();
+                scroll_start = ($(this).scrollTop()>0);
                 if(scroll_start > offset.top) {
                     $('.nav1').css('background-color', 'rgba(3, 56, 69, 0.95)');
                 } else {
                     $('.nav1').css('background-color', 'transparent');
                 }
             });
-        });
-        $(document).ready(function(){
+//        });
+//        $(document).ready(function(){
 
             //Check to see if the window is top if not then display button
             $(window).scroll(function(){
@@ -267,51 +255,6 @@
             });
         });
 
-
-
-
-
-//        $(function() {
-//
-//            $('a[href*=#]').on('click', function(e) {
-//                e.preventDefault();
-//                $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 3000);
-//
-//            });
-//        });
-
-    </script>
-    <script>
-        jQuery(function ($) {
-            $(window).load(function(){
-                setTimeout(function(){
-                    var myCircle = Circles.create({
-                        id:                  'circles-309',
-                        radius:              83,
-                        value:               0,
-                        maxValue:            100,
-                        width:               10,
-                        text:                function(value){
-                            return '<div class="text-inner">' + value + '<span class="small">%</span>' +
-                                    '<span class="text">Complete</span></div>';
-                        },
-                        colors:              ['#FFF', '#f8b864'],
-                        duration:            600,
-                        wrpClass:            'circles-wrp',
-                        textClass:           'circles-text',
-                        valueStrokeClass:    'circles-valueStroke',
-                        maxValueStrokeClass: 'circles-maxValueStroke',
-                        styleWrapper:        true,
-                        styleText:           true
-                    });
-                }, 3000);
-
-            });
-        });
-
-    </script>
-    <script type="text/javascript">
-
         $(document).ready(function(){
 
             $("#suggestform").bootstrapValidator({
@@ -351,7 +294,7 @@
             $(this).prop('Counter',0).animate({
                 Counter: $(this).text()
             }, {
-                duration: 3000,
+                duration: 1000,
                 easing: 'swing',
                 step: function (now) {
                     $(this).text(Math.ceil(now));
@@ -362,8 +305,5 @@
 
     </script>
 
-    {{--<script type="text/javascript" id="hcb">--}}
-    {{--/*<!--*/ if(!window.hcb_user){hcb_user={};} hcb_user.PAGE="http://localhost:8000/";--}}
-    {{--(function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%244TFKaKX4UR2naHrnkpaM7."+"&opts=86&num=2&ts=1475256747035");--}}
-    {{--if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); hcb_user.submit="submit";/*-->*/ </script>--}}
+
 @endsection
