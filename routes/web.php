e5bcf1bf1fb0de53d80281ff260936d17982624e
sandeepyadavm <sandeepyadvam@ziontech.com>
2016-10-20 13:02:41 -0700

    pagination fixed for event and projects

--- routes/web.php
+++ routes/web.php
@@ -66,12 +66,14 @@ Route::get('/home', 'HomeController@index');
  */
 
 Route::post('admin/users/search ', 'AdminController@searchUser');
+Route::get('admin/users/search', 'AdminController@getAllUsers');
 Route::get('events/lists/all', 'EventController@allEvents');
 Route::post('events/page/get/', 'EventController@paginateEvents');
 Route::get('events/lists/count', 'EventController@getEventCount');
 Route::get('events/status/current', 'EventController@getCurrentEvents');
 Route::get('events/status/future', 'EventController@getFutureEvents');
-
+Route::post('events/page/future/{id}', 'EventController@paginateUpcomingEvents'); //Get pagination for future events
+Route::post('events/page/current/{id}', 'EventController@paginateCurrentEvents'); //Get pagination for current events
 /**
  * json url project
  */
@@ -80,7 +82,8 @@ Route::post('projects/page/{id}', 'ProjectController@paginateProjects');
 Route::get('projects/status/current', 'ProjectController@getCurrentProject');
 Route::get('projects/status/future', 'ProjectController@getFutureProject');
 Route::get('projects/lists/count', 'ProjectController@getProjectCount');
-
+Route::get('projects/page/future/{id}', 'ProjectController@paginateUpcomingProjects'); //Get pagination for future Projects
+Route::get('projects/page/current/{id}', 'ProjectController@paginateCurrentProjects'); //Get pagination for current projects
 
 /**
  *
