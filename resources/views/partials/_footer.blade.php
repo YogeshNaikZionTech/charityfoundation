58de7a16ec3d1cc510d58c9f8c8ec3b1f47e62f4
sravanthirk <sravanthik@ziontech.com>
2016-10-21 12:59:52 -0700

    Admin Panel Changes

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,5 +1,5 @@
  <footer id="footer">
-    <div class="container-fluid">
+    <div class="container-fluid footerContainer">
         <div class="row">
             <div class="col-md-3 col-lg-3 col-xs-10 col-sm-3">
                 <span class="copyright">Copyright &copy; AA Foundation 2016</span>
@@ -69,5 +69,6 @@
 
              $('#footer').css('margin-top', $(document).height() - $('#content').height() - $('#footer').height());
          });
+
      </script>
      @endsection
