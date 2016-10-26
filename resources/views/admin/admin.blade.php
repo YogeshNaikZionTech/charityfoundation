29f2cb2e4432d29ad57fbc1a8f7157db7f2681f2
sravanthirk <sravanthik@ziontech.com>
2016-10-26 12:35:31 -0700

    Admin Enhancement

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -402,41 +402,7 @@
 
 @endsection
 @section('scripts')
-    <script>
-        $(document).ready(function(){
-            $(this).scrollTop(0);
-        });
-        $(document).ready(function(){
-            var scroll_start = 1;
-            var startChange = $('.nav1');
-            var offset = startChange.offset();
-            $(document).scroll(function() {
-                scroll_start = ($(this).scrollTop()>0);
-                if(scroll_start > offset.top) {
-                    $('.nav1').css('background-color', '#2c7873');
-                } else {
-                    $('.nav1').css('background-color', 'transparent');
-                }
-            });
-//        });
-//        $(document).ready(function(){
-
-            //Check to see if the window is top if not then display button
-            $(window).scroll(function(){
-                if ($(this).scrollTop() > 100) {
-                    $('.scrollToTop').fadeIn();
-                } else {
-                    $('.scrollToTofp').fadeOut();
-                }
-            });
-            //Click event to scroll to top
-            $('.scrollToTop').click(function(){
-                $('html, body').animate({scrollTop : 0},1000);
-                return false;
-            });
-        });
-    </script>
-
+<script src="js/nav.js" type="text/javascript"></script>
     <script type="text/javascript">
     $(document).ready(function()
     {
