e4d3c4ffc0061cfe67606333afa957869223d11b
kimsuka <kimsukar@ziontech.com>
2016-11-22 16:01:27 -0800

    projects updated with real-time content

--- database/seeds/projectTabelSeeder.php
+++ database/seeds/projectTabelSeeder.php
@@ -12,67 +12,51 @@ class projectTabelSeeder extends Seeder
      */
     public function run()
     {
- // DB::table( 'project' )->insert( [
 
-	// 			    'project_Title'       => 'Education Programs',
-	// 			    'project_Description' => 'We reach out to young minds and give them exposure to various topics in the field of science, math and language in an effort to introduce and inspire children to pursue a certain field. A survey was conducted in the villages where parents where asked what they think their children are missing out on. Majority of them wanted their children to learn to speak in the English language. We provide these schools with language teachers who teach the children how to speak in English.' ,
-	// 			    'project_Date'        => \Carbon\Carbon::today(),
-	// 			    'project_Location'     => 'Cherlapalem, Telangana',
-	// 			    'project_StartTime'  => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			        'created_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			    'updated_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			    'project_Image'   => 'project1.png',
-	// 			    'project_Status'  => 'Current',
-	// 	    ] );
+ DB::table( 'project' )->insert( [
 
- // DB::table( 'project' )->insert( [
-
-	// 			    'project_Title'       => 'Green Schools Initiative',
-	// 			    'project_Description' => 'AA Foundation believes in making a change towards the betterment of the environment in any small way possible because every small step taken towards conservation will have an impact. In our effort to go green, we provide solar panels to schools making them more sustainable and reducing the energy costs to utilize funds for the betterment of quality of education. We eventually aim to make them self-sustainable and acquaint the future generation to green lifestyle from a young impressionable age. ' ,
-	// 			    'project_Date'        => \Carbon\Carbon::today(),
-	// 			    'project_Location'     => 'Cherlapalem, Telangana',
-	// 			    'project_StartTime'  => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			        'created_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			    'updated_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			    'project_Image'   => 'project2.png',
-	// 			    'project_Status'  => 'Current',
-	// 	    ] );
-
- //  DB::table( 'project' )->insert( [
-
-	// 			    'project_Title'       => 'Classroom Digitization',
-	// 			    'project_Description' => 'The need for technology in the field of education is clearly pronounced. Yet, majority of children in rural India are denied this necessity. AA Foundation strives to help the children from less fortunate economic backgrounds to keep up with this trend.  We provide projectors etc etc etc to schools in rural areas to give the children a more visual learning experience and improving the quality of learning. Help us help children get acquainted with technology from early on.' ,
-	// 			    'project_Date'        => \Carbon\Carbon::today(),
-	// 			    'project_Location'     => 'Cherlapalem, Telangana',
-	// 			    'project_StartTime'  => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			        'created_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			    'updated_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-	// 			    'project_Image'   => 'project3.png',
-	// 			    'project_Status'  => 'Current',
-	// 	    ] );
-
-		
-		    $Faker = Faker::create( 'App\Project' );
-		        for ( $i = 0; $i < 10; $i ++ ) {
-			    DB::table( 'project' )->insert( [
-
-				    'project_Title'       => 'Education for a region',
-				    'project_Description' => $Faker->paragraph(15),
+				    'project_Title'       => 'Education Programs',
+				    'project_Description' => 'We reach out to young minds and give them exposure to various topics in the field of science, math and language in an effort to introduce and inspire children to pursue a certain field. A survey was conducted in the villages where parents where asked what they think their children are missing out on. Majority of them wanted their children to learn to speak in the English language. We provide these schools with language teachers who teach the children how to speak in English.' ,
 				    'project_Date'        => \Carbon\Carbon::today(),
-				    'project_Location'     => 'barbodos',
+				    'project_Location'     => 'Cherlapalem, Andhra Pradesh',
 				    'project_StartTime'  => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
 				        'created_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
 				    'updated_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
-				    'project_Image'   => 'charity.jpg',
+				    'project_Image'   => 'project1.png',
 				    'project_Status'  => 'Current',
+		    ] );
+  DB::table( 'project' )->insert( [
 
+				    'project_Title'       => 'Classroom Digitization',
+				    'project_Description' => 'The need for technology in the field of education is clearly pronounced. Yet, majority of children in rural India are denied this necessity. AA Foundation strives to help the children from less fortunate economic backgrounds to keep up with this trend.  We provide projectors etc etc etc to schools in rural areas to give the children a more visual learning experience and improving the quality of learning. Help us help children get acquainted with technology from early on.' ,
+				    'project_Date'        => \Carbon\Carbon::today(),
+				    'project_Location'     => 'Warangal District, Telangana',
+				    'project_StartTime'  => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
+				        'created_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
+				    'updated_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
+				    'project_Image'   => 'project3.png',
+				    'project_Status'  => 'Current',
+		    ] );
 
-			    ] );
-		    }
+ DB::table( 'project' )->insert( [
 
-	    for ( $i = 0; $i < 10; $i ++ ) {
+				    'project_Title'       => 'Green Schools Initiative',
+				    'project_Description' => 'AA Foundation believes in making a change towards the betterment of the environment in any small way possible because every small step taken towards conservation will have an impact. In our effort to go green, we provide solar panels to schools making them more sustainable and reducing the energy costs to utilize funds for the betterment of quality of education. We eventually aim to make them self-sustainable and acquaint the future generation to green lifestyle from a young impressionable age. ' ,
+				    'project_Date'        => \Carbon\Carbon::today(),
+				    'project_Location'     => 'Hyderabad, India',
+				    'project_StartTime'  => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
+				        'created_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
+				    'updated_at'      => \Carbon\Carbon::now()->format( 'Y-m-d H:i:s' ),
+				    'project_Image'   => 'project2.png',
+				    'project_Status'  => 'Current',
+		    ] );
+
+ 
+		      
+	    $Faker = Faker::create( 'App\Project' );
+
+	    for ( $i = 0; $i < 1; $i ++ ) {
 		    DB::table( 'project' )->insert( [
-
 			    'project_Title'       => $Faker->sentence,
 			    'project_Description' => $Faker->paragraph(15),
 			    'project_Date'        => \Carbon\Carbon::today(),
@@ -83,12 +67,10 @@ class projectTabelSeeder extends Seeder
 			    'project_Image'   => 'upcoming.jpg',
 			    'project_Status'  => 'future',
 			    
-
 		    ] );
 	    }
-	    for ( $i = 0; $i < 10; $i ++ ) {
+	    for ( $i = 0; $i < 1; $i ++ ) {
 		    DB::table( 'project' )->insert( [
-
 			    'project_Title'       => $Faker->sentence,
 			    'project_Description' => $Faker->paragraph(15),
 			    'project_Date'        => \Carbon\Carbon::today(),
@@ -99,9 +81,7 @@ class projectTabelSeeder extends Seeder
 			    'project_Image'   => 'Charity.jpg',
 			    'project_Status'  => 'completed',
 			    
-
 		    ] );
 	    }
-
-    }
+	}
 }
